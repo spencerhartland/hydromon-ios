0e497063d48aeed505c49660df3faf22ef7043db
Spencer Hartland <spencerhartland@icloud.com>
2023-03-20 03:58:24 -0700

    - Solved a warning about publishing values on background threads. - Added logic to `updatePreference`

--- Hydromon/Model/ContentView-ViewModel.swift
+++ Hydromon/Model/ContentView-ViewModel.swift
@@ -16,13 +16,43 @@ extension ContentView {
         
         func testConnection() {
             httpRequestHandler.testConnection { success, response in
+                if !success {
+                    print(response)
+                    return
+                }
                 self.connected = true
-                if !success { print(response) }
             }
         }
         
         func updatePreference(_ preference: PreferenceSet.PreferenceKey, value: Any) {
-            // TODO: Implement logic to update any preference in PrefereceSet
+            switch preference {
+            case .LCDStandbyMessage:
+                self.preferences.LCDStandbyMessage = value as! String
+            case .LCDAlertMessage:
+                self.preferences.LCDAlertMessage = value as! String
+            case .LCDStandbyColor:
+                self.preferences.LCDStandbyColor = value as! RGB
+            case .LCDAlertColor:
+                self.preferences.LCDAlertColor = value as! RGB
+            case .LEDStandbyColor:
+                self.preferences.LEDStandbyColor = value as! RGB
+            case .LEDAlertColor:
+                self.preferences.LEDAlertColor = value as! RGB
+            case .standbyTimeout:
+                self.preferences.standbyTimeout = value as! Int
+            case .alertTimeout:
+                self.preferences.alertTimeout = value as! Int
+            case .alertDelay:
+                self.preferences.alertDelay = value as! Int
+            case .OLEDMaxBrightness:
+                self.preferences.OLEDMaxBrightness = value as! Int
+            case .sipSize:
+                self.preferences.sipSize = value as! Int
+            case .localNetworkSSID:
+                self.preferences.localNetworkSSID = value as! String
+            case .localNetworkPassword:
+                self.preferences.localNetworkPassword = value as! String
+            }
             return
         }
     }
