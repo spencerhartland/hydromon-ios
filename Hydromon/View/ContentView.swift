968880132ff72dad595b0f8d742d3fdb4d09918e
Spencer Hartland <spencerhartland@icloud.com>
2023-03-24 02:14:31 -0700

    Added all of the rest of the preference controls, embedded them inside a `ScrollView`.

--- Hydromon/View/ContentView.swift
+++ Hydromon/View/ContentView.swift
@@ -14,8 +14,20 @@ struct ContentView: View {
     private let textfieldDisclosureBackgroundImageName = "control_textfield-disclosure-bg"
     private let textfieldDisclosureHighlightImageName = "control_textfield-disclosure-highlight"
     private let disclosureIcon = "chevron.right"
+    private let lcdStandbyMessgageTitle = "LCD Standby Message"
     private let lcdStandbyMessageFooter = "This message will be displayed on the LCD during standby when you pick up your Hydromon."
-    private let lcdStandbyMessgageControlTitle = "LCD Standby Message"
+    private let lcdAlertMessageTitle = "LCD Alert Message"
+    private let lcdAlertMessageFooter = "This message will be displayed on the LCD during a drink reminder alert."
+    private let standbyTimeoutTitle = "Standby Timeout"
+    private let standbyTimeoutFooter = "The amount of time the device will stay awake after being picked up, in seconds."
+    private let alertTimeoutTitle = "Alert Timeout"
+    private let alertTimeoutFooter = "The duration of a drink reminder alert, in seconds."
+    private let alertDelayTitle = "Alert Delay"
+    private let alertDelayFooter = "The delay between drink reminder alerts, in seconds."
+    private let oledMaxBrightnessTitle = "OLED Max Brightness"
+    private let oledMaxBrightnessFooter = "The maximum brightness of the fill level display. Maximum brightness: 255."
+    private let sipSizeTitle = "Sip Size"
+    private let sipSizeFooter = "The average amount of water consumed in one 'sip,' in milileters."
     
     @StateObject private var viewModel = ViewModel()
     
@@ -39,6 +51,7 @@ struct ContentView: View {
                         HydromonStatusView(viewModel: .init(), LCD: $viewModel.preferences.LCDStandbyColor, statusLED: $viewModel.preferences.LEDStandbyColor)
                         // Controls
                         controls
+                            .ignoresSafeArea()
                     } else {
                         Spacer()
                         ConnectionProblemView {
@@ -47,7 +60,6 @@ struct ContentView: View {
                         Spacer()
                     }
                 }
-                Spacer()
             }
         }
         .onAppear {
@@ -56,56 +68,110 @@ struct ContentView: View {
     }
     
     private var controls: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            // COLORS
-            // LCD
-            HStack {
-                // STANDBY
-                NavigationLink {
-                    CustomColorPicker(component: .LCD, mode: .standby) { rgb in
-                        self.viewModel.updatePreference(.LCDStandbyColor, value: rgb)
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading, spacing: 16) {
+                // COLORS
+                // LCD
+                HStack {
+                    // STANDBY
+                    NavigationLink {
+                        CustomColorPicker(component: .LCD, mode: .standby) { rgb in
+                            self.viewModel.updatePreference(.LCDStandbyColor, value: rgb)
+                        }
+                        .navigationBarBackButtonHidden()
+                    } label: {
+                        IndicatorButton(title: "LCD STANDBY", rgb: $viewModel.preferences.LCDStandbyColor)
                     }
-                    .navigationBarBackButtonHidden()
-                } label: {
-                    IndicatorButton(title: "LCD STANDBY", rgb: $viewModel.preferences.LCDStandbyColor)
+                    // ALERT
+                    NavigationLink {
+                        CustomColorPicker(component: .LCD, mode: .alert) { rgb in
+                            self.viewModel.updatePreference(.LCDAlertColor, value: rgb)
+                        }
+                        .navigationBarBackButtonHidden()
+                    } label: {
+                        IndicatorButton(title: "LCD ALERT", rgb: $viewModel.preferences.LCDAlertColor)
+                    }
                 }
-                // ALERT
-                NavigationLink {
-                    CustomColorPicker(component: .LCD, mode: .alert) { rgb in
-                        self.viewModel.updatePreference(.LCDAlertColor, value: rgb)
+                // STATUS LED
+                HStack {
+                    // STANDBY
+                    NavigationLink {
+                        CustomColorPicker(component: .LED, mode: .standby) { rgb in
+                            self.viewModel.updatePreference(.LEDStandbyColor, value: rgb)
+                        }
+                        .navigationBarBackButtonHidden()
+                    } label: {
+                        IndicatorButton(title: "LED STANDBY", rgb: $viewModel.preferences.LEDStandbyColor)
                     }
-                    .navigationBarBackButtonHidden()
+                    // ALERT
+                    NavigationLink {
+                        CustomColorPicker(component: .LED, mode: .alert) { rgb in
+                            self.viewModel.updatePreference(.LEDAlertColor, value: rgb)
+                        }
+                        .navigationBarBackButtonHidden()
+                    } label: {
+                        IndicatorButton(title: "LED ALERT", rgb: $viewModel.preferences.LEDAlertColor)
+                    }
+                }
+                .padding(.bottom, 16)
+                // LCD STANDBY MESSAGE
+                NavigationLink {
+                    PreferenceTextfieldView(title: lcdStandbyMessgageTitle, preference: $viewModel.preferences.LCDStandbyMessage)
+                        .navigationBarBackButtonHidden()
                 } label: {
-                    IndicatorButton(title: "LCD ALERT", rgb: $viewModel.preferences.LCDAlertColor)
+                    textfieldDisclosure(title: lcdStandbyMessgageTitle, currentValue: viewModel.preferences.LCDStandbyMessage, footer: lcdStandbyMessageFooter)
+                }
+                // LCD ALERT MESSAGE
+                NavigationLink {
+                    PreferenceTextfieldView(title: lcdAlertMessageTitle, preference: $viewModel.preferences.LCDAlertMessage)
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    textfieldDisclosure(title: lcdAlertMessageTitle, currentValue: viewModel.preferences.LCDAlertMessage, footer: lcdAlertMessageFooter)
+                }
+                // STANDBY TIMEOUT
+                NavigationLink {
+                    PreferenceNumberEntryView(title: standbyTimeoutTitle, footer: standbyTimeoutFooter, preference: $viewModel.preferences.standbyTimeout)
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    textfieldDisclosure(title: standbyTimeoutTitle, currentValue: String(viewModel.preferences.standbyTimeout), footer: standbyTimeoutFooter)
+                }
+                // ALERT TIMEOUT
+                NavigationLink {
+                    PreferenceNumberEntryView(title: alertTimeoutTitle, footer: alertTimeoutFooter, preference: $viewModel.preferences.alertTimeout)
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    textfieldDisclosure(title: alertTimeoutTitle, currentValue: String(viewModel.preferences.alertTimeout), footer: alertTimeoutFooter)
+                }
+                // ALERT DELAY
+                NavigationLink {
+                    PreferenceNumberEntryView(title: alertDelayTitle, footer: alertDelayFooter, preference: $viewModel.preferences.alertDelay)
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    textfieldDisclosure(title: alertDelayTitle, currentValue: String(viewModel.preferences.alertDelay), footer: alertDelayFooter)
+                }
+                // OLED MAX BRIGHTNESS
+                NavigationLink {
+                    PreferenceNumberEntryView(title: oledMaxBrightnessTitle, footer: oledMaxBrightnessFooter, preference: $viewModel.preferences.OLEDMaxBrightness)
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    textfieldDisclosure(title: oledMaxBrightnessTitle, currentValue: String(viewModel.preferences.OLEDMaxBrightness), footer: oledMaxBrightnessFooter)
+                }
+                // SIP SIZE
+                NavigationLink {
+                    PreferenceNumberEntryView(title: sipSizeTitle, footer: sipSizeFooter, preference: $viewModel.preferences.sipSize)
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    textfieldDisclosure(title: sipSizeTitle, currentValue: String(viewModel.preferences.sipSize), footer: sipSizeFooter)
                 }
             }
-            // STATUS LED
-            HStack {
-                // STANDBY
-                NavigationLink {
-                    CustomColorPicker(component: .LED, mode: .standby) { rgb in
-                        self.viewModel.updatePreference(.LEDStandbyColor, value: rgb)
-                    }
-                    .navigationBarBackButtonHidden()
-                } label: {
-                    IndicatorButton(title: "LED STANDBY", rgb: $viewModel.preferences.LEDStandbyColor)
-                }
-                // ALERT
-                NavigationLink {
-                    CustomColorPicker(component: .LED, mode: .alert) { rgb in
-                        self.viewModel.updatePreference(.LEDAlertColor, value: rgb)
-                    }
-                    .navigationBarBackButtonHidden()
-                } label: {
-                    IndicatorButton(title: "LED ALERT", rgb: $viewModel.preferences.LEDAlertColor)
-                }
-            }
-            NavigationLink {
-                PreferenceTextfieldView(title: lcdStandbyMessgageControlTitle, preference: $viewModel.preferences.LCDStandbyMessage)
-                    .navigationBarBackButtonHidden()
-            } label: {
-                textfieldDisclosure(title: lcdStandbyMessgageControlTitle, currentValue: viewModel.preferences.LCDStandbyMessage, footer: lcdStandbyMessageFooter)
-            }
+            .padding(.top, 16)
+            .padding(.bottom, 24)
+        }
+        .mask {
+            LinearGradient(stops: [
+                .init(color: .clear, location: 0.01),
+                .init(color: .black, location: 0.04)
+            ], startPoint: .top, endPoint: .bottom)
         }
     }
     
@@ -151,7 +217,7 @@ struct ContentView: View {
     }
 }
 
-struct RootView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .background {
@@ -159,5 +225,6 @@ struct RootView_Previews: PreviewProvider {
                     .ignoresSafeArea()
             }
             .preferredColorScheme(.dark)
+            .edgesIgnoringSafeArea(.bottom)
     }
 }
