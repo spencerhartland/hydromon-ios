9d0319b5f82e89167b549948132d223cb7d4f3c3
Spencer Hartland <spencerhartland@icloud.com>
2023-03-27 08:31:45 -0700

    Refactored `ConnectionStatusView` to fix a bug that caused the connection status indicator to remain red even when connected.

--- Hydromon/View/Status/ConnectionStatusView.swift
+++ Hydromon/View/Status/ConnectionStatusView.swift
@@ -13,27 +13,25 @@ struct ConnectionStatusView: View {
     private let notConnectedText = "Not Connected"
     
     @Binding var connected: Bool
-    @State var connectionStatusIndicatorColor: Color
     // Animation
     @State var animate: Bool = false
     @State var fadeOutRadar: Bool = false
     
     init(_ connected: Binding<Bool>) {
         self._connected = connected
-        self.connectionStatusIndicatorColor = connected.wrappedValue ? Colors.accent : .red
     }
     
     var body: some View {
         HStack(spacing: 2) {
             ZStack {
                 Circle()
-                    .foregroundColor(connectionStatusIndicatorColor.opacity(fadeOutRadar ? 0.0 : 0.5))
+                    .foregroundColor(indicatorColor().opacity(fadeOutRadar ? 0.0 : 0.5))
                     .frame(height: connected ? (animate ? 24 : 0) : 0)
                 Image(systemName: symbolName)
                     .font(.system(size: 8))
-                    .foregroundColor(connectionStatusIndicatorColor)
-                    .glow(connectionStatusIndicatorColor)
-                    .onAppear {
+                    .foregroundColor(indicatorColor())
+                    .glow(indicatorColor())
+                    .onChange(of: connected) { _ in
                         withAnimation(.linear(duration: 1.5).repeatForever(autoreverses: false)) {
                             if connected { self.animate.toggle() }
                         }
@@ -47,6 +45,10 @@ struct ConnectionStatusView: View {
                 .font(.system(size: 8, weight: .bold, design: .monospaced))
                 .foregroundColor(Colors.secondary)
         }
+    }
+    
+    func indicatorColor() -> Color {
+        return self.connected ? Colors.accent : .red
     }
 }
 
