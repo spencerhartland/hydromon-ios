9ad3732cb8938fc93c942eeae5fb2ab61df6e404
Spencer Hartland <spencerhartland@icloud.com>
2023-05-31 00:43:03 -0700

    Deleted `PreferenceSet.swift` as it will not be used when `BluetoothManager` replaces network code.

--- Hydromon/Common/RGB.swift
+++ Hydromon/Common/RGB.swift
@@ -13,7 +13,26 @@ struct RGB: Codable {
     var green: Int
     var blue: Int
     
+    public init(red: Int, green: Int, blue: Int) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+    
+    public init(from hexString: String) {
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexString).scanHexInt64(&rgbValue)
+        
+        self.red = Int((rgbValue & 0xFF0000) >> 16)
+        self.green = Int((rgbValue & 0x00FF00) >> 8)
+        self.blue = Int(rgbValue & 0x0000FF)
+    }
+    
     public func color() -> Color {
         return Color(red: (Double(self.red) / 255), green: (Double(self.green) / 255), blue: (Double(self.blue) / 255))
+    }
+    
+    public func hexString() -> String {
+        return String(format: "%02X%02X%02X", self.red, self.green, self.blue)
     }
 }
