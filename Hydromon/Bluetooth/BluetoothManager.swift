7d3896015ebc79389c2121e46fb72b21e3e80bdf
Spencer Hartland <spencerhartland@icloud.com>
2023-05-31 00:38:43 -0700

    Updates to `BluetoothManager` and `PreferencesManager` to store individual references to each preference and enable writing to Hydromon upon changes to any preference.

--- Hydromon/Bluetooth/BluetoothManager.swift
+++ Hydromon/Bluetooth/BluetoothManager.swift
@@ -23,11 +23,12 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CB
     /// A boolean value indicating whether or not this device is connected to a Hydromon device.
     @Published var isConnected: Bool = false
     /// An instance of `PreferencesManager` which manages a collection of user preferences.
-    @Published var preferences = PreferencesManager()
+    @Published var preferences: PreferencesManager!
     
     override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+        preferences = PreferencesManager(self.write)
     }
     
     // MARK: - BluetoothManager
@@ -44,6 +45,17 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CB
         centralManager.stopScan()
     }
     
+    /// Tell the central manager to retrieve previously connected peripherals and connect again automatically.
+    func reconnect() {
+        if let savedPeripheralIDString = UserDefaults.standard.string(forKey: deviceUUIDUserDefaultsKey),
+           let savedPeripheralID = UUID(uuidString: savedPeripheralIDString) {
+            let knownPeripherals = centralManager.retrievePeripherals(withIdentifiers: [savedPeripheralID])
+            if knownPeripherals.count > 0 {
+                connect(peripheral: knownPeripherals[0])
+            }
+        }
+    }
+    
     /// Tells the central manager to attempt to connect to the specified peripheral.
     func connect(peripheral: CBPeripheral) {
         self.peripheral.delegate = self
@@ -52,10 +64,13 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CB
     
     /// Writes the provided value to the specified characteristic.
     func write(_ value: String, to uuid: CBUUID) {
+        print("write called")
         if let data = value.data(using: .utf8) {
+            print("got data")
             do {
                 let characteristic = try characteristics.getCharacteristic(with: uuid)
-                self.peripheral.writeValue(data, for: characteristic, type: .withoutResponse)
+                print("attempting write, uuid: \(characteristic.uuid.uuidString)")
+                self.peripheral.writeValue(data, for: characteristic, type: .withResponse)
             } catch {
                 print("Invalid characteristic ID")
             }
@@ -86,6 +101,8 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CB
         if peripheral == self.peripheral {
             if debug { print("Connected to your Hydromon") }
             isConnected = true
+            let peripheralUUID = peripheral.identifier.uuidString
+            UserDefaults.standard.set(peripheralUUID, forKey: deviceUUIDUserDefaultsKey)
             peripheral.discoverServices(nil)
         }
     }
@@ -135,7 +152,42 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CB
         if let data = characteristic.value {
             if let value = String(data: data, encoding: .utf8) {
                 if debug { print("Value: \(value)") }
-                preferences.update(preference: characteristic.uuid, value: value)
+                switch characteristic.uuid {
+                case HydromonUUIDs.DeviceInformation.manufacturersNameID:
+                    preferences.manufacturersName = value
+                case HydromonUUIDs.DeviceInformation.modelNumberID:
+                    preferences.modelNumber = value
+                case HydromonUUIDs.DeviceInformation.firmwareRevisionID:
+                    preferences.firmwareRevision = value
+                case HydromonUUIDs.DeviceInformation.softwareRevisionID:
+                    preferences.softwareRevision = value
+                case HydromonUUIDs.Preferences.lcdStandbyMessageID:
+                    preferences.lcdStandbyMessage = value
+                case HydromonUUIDs.Preferences.lcdAlertMessageID:
+                    preferences.lcdAlertMessage = value
+                case HydromonUUIDs.Preferences.lcdStandbyColorID:
+                    preferences.lcdStandbyColor = value
+                case HydromonUUIDs.Preferences.lcdAlertColorID:
+                    preferences.lcdAlertColor = value
+                case HydromonUUIDs.Preferences.ledStandbyColorID:
+                    preferences.ledStandbyColor = value
+                case HydromonUUIDs.Preferences.ledAlertColorID:
+                    preferences.ledAlertColor = value
+                case HydromonUUIDs.Preferences.standbyTimeoutID:
+                    preferences.standbyTimeout = Int(value) ?? 0
+                case HydromonUUIDs.Preferences.alertTimeoutID:
+                    preferences.alertTimeout = Int(value) ?? 0
+                case HydromonUUIDs.Preferences.alertDelayID:
+                    preferences.alertDelay = Int(value) ?? 0
+                case HydromonUUIDs.Preferences.oledMaxBrightnessID:
+                    preferences.oledMaxBrightness = Int(value) ?? 0
+                case HydromonUUIDs.Preferences.sipSizeID:
+                    preferences.sipSize = Int(value) ?? 0
+                default:
+                    // Unknown
+                    if debug { print("Unknown characterisitc: \(characteristic.uuid)") }
+                    break
+                }
             } else {
                 print("There was an error casting the characteristic's value as a String.")
             }
--- Hydromon/Bluetooth/PreferencesManager.swift
+++ Hydromon/Bluetooth/PreferencesManager.swift
@@ -8,29 +8,122 @@
 import Foundation
 import CoreBluetooth
 
-enum PreferencesManagerError: Error {
-    case InvalidPreferenceID
-}
-
 /// Manages a collection of user preferences.
-class PreferencesManager {
-    private var preferences: [CBUUID: String] = [:]
+class PreferencesManager: ObservableObject {
+    private static let defaultString = ""
+    private static let defaultColor = "ffffff"
+    private static let defaultInt = 0
     
-    /// Provides functionality for adding or modifying preference values to the collection.
-    public func update(preference id: CBUUID, value: String) {
-        preferences[id] = value
+    @Published var manufacturersName: String = defaultString
+    @Published var modelNumber: String = defaultString
+    @Published var firmwareRevision: String = defaultString
+    @Published var softwareRevision: String = defaultString
+    @Published var lcdStandbyMessage: String = defaultString {
+        didSet {
+            if oldValue != lcdStandbyMessage && oldValue != PreferencesManager.defaultString {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(lcdStandbyMessage, HydromonUUIDs.Preferences.lcdStandbyMessageID)
+                }
+            }
+        }
+    }
+    @Published var lcdAlertMessage: String = defaultString {
+        didSet {
+            if oldValue != lcdAlertMessage && oldValue != PreferencesManager.defaultString {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(lcdAlertMessage, HydromonUUIDs.Preferences.lcdAlertMessageID)
+                }
+            }
+        }
+    }
+    @Published var lcdStandbyColor: String = defaultColor {
+        didSet {
+            print("didSet called")
+            if oldValue != lcdStandbyColor && oldValue != PreferencesManager.defaultColor {
+                print("Old value: \(oldValue), new value: \(lcdStandbyColor)")
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    print("calling onPreferenceUpdate...")
+                    onPreferenceUpdate(lcdStandbyColor, HydromonUUIDs.Preferences.lcdStandbyColorID)
+                }
+            }
+        }
+    }
+    @Published var lcdAlertColor: String = defaultColor {
+        didSet {
+            if oldValue != lcdAlertColor && oldValue != PreferencesManager.defaultColor {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(lcdAlertColor, HydromonUUIDs.Preferences.lcdAlertColorID)
+                }
+            }
+        }
+    }
+    @Published var ledStandbyColor: String = defaultColor {
+       didSet {
+           if oldValue != ledStandbyColor && oldValue != PreferencesManager.defaultColor {
+               if let onPreferenceUpdate = onPreferenceUpdate {
+                   onPreferenceUpdate(ledStandbyColor, HydromonUUIDs.Preferences.ledStandbyColorID)
+               }
+           }
+       }
+    }
+    @Published var ledAlertColor: String = defaultColor {
+        didSet {
+            if oldValue != ledAlertColor && oldValue != PreferencesManager.defaultColor {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(ledAlertColor, HydromonUUIDs.Preferences.ledAlertColorID)
+                }
+            }
+        }
+    }
+    @Published var standbyTimeout: Int = defaultInt {
+        didSet {
+            if oldValue != standbyTimeout && oldValue != PreferencesManager.defaultInt {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(String(standbyTimeout), HydromonUUIDs.Preferences.standbyTimeoutID)
+                }
+            }
+        }
+    }
+    @Published var alertTimeout: Int = defaultInt {
+        didSet {
+            if oldValue != alertTimeout && oldValue != PreferencesManager.defaultInt {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(String(alertTimeout), HydromonUUIDs.Preferences.alertTimeoutID)
+                }
+            }
+        }
+    }
+    @Published var alertDelay: Int = defaultInt {
+        didSet {
+            if oldValue != alertDelay && oldValue != PreferencesManager.defaultInt {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(String(alertDelay), HydromonUUIDs.Preferences.alertDelayID)
+                }
+            }
+        }
+    }
+    @Published var oledMaxBrightness: Int = defaultInt {
+        didSet {
+            if oldValue != oledMaxBrightness && oldValue != PreferencesManager.defaultInt {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(String(oledMaxBrightness), HydromonUUIDs.Preferences.oledMaxBrightnessID)
+                }
+            }
+        }
+    }
+    @Published var sipSize: Int = defaultInt {
+        didSet {
+            if oldValue != sipSize && oldValue != PreferencesManager.defaultInt {
+                if let onPreferenceUpdate = onPreferenceUpdate {
+                    onPreferenceUpdate(String(sipSize), HydromonUUIDs.Preferences.sipSizeID)
+                }
+            }
+        }
     }
     
-    /**
-     Returns the value of the preference with the specified UUID. If the UUID is not
-     present within the collection, throws an `InvalidPreferenceID` error.
-     
-     - Returns: The value of the preference with the specified UUID
-     */
-    public func getValue(for preference: CBUUID) throws -> String {
-        guard let value = preferences[preference] else {
-            throw PreferencesManagerError.InvalidPreferenceID
-        }
-        return value
+    let onPreferenceUpdate: ((String, CBUUID) -> Void)?
+    
+    public init(_ handler: ((String, CBUUID) -> Void)? = nil) {
+        self.onPreferenceUpdate = handler
     }
 }
